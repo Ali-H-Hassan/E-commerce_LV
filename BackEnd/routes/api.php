3ea6977bf636ac45242dc3d65ea89008533eb316
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-10 04:05:40 +0200

    Product CRUD

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -1,19 +1,28 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShoppingCartController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/products', 'get_products');
+    Route::post('/products', 'add_product');
+    Route::post('/products/update/{id}', 'update_product');
+    Route::delete('/products/{id}', 'delete_product');
+});
+
+Route::controller(ShoppingCartController::class)->group(function () {
+    Route::get('/cart', 'get_shopping_cart');
+    Route::post('/cart', 'add_shopping_cart');
+    Route::delete('/cart', 'delete_shopping_cart');
 });
